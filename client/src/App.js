595c10a1235b975fc0ce669a5b2453911dfcee12
Mozl <louis.moselhi@bbc.co.uk>
2020-12-19 18:20:22 +0100

    move provider, update test

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Launches from "./components/Launches";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='App'>
-      <h1>SpaceX</h1>
-      <Launches />
-    </div>
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <h1>SpaceX</h1>
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
